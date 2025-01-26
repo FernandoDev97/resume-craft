5a1998aab134bdb3f7e494a942037f6b53708c98
Fernando Souza <l.fernando.dev@gmail.com>
2025-01-26 13:48:53 -0300

    feat: finish resume page

--- src/app/dashboard/resumes/[id]/_components/resume-content/index..tsx
+++ src/app/dashboard/resumes/[id]/_components/resume-content/index..tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import { useFormContext } from 'react-hook-form'
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 
 import { TransformControls } from './controls'
 import { NavigationHeader } from './header'
 import { ResumeTemplate } from './templates'
 export const ResumeContent = () => {
+  const { watch } = useFormContext<ResumeData>()
+
+  const data = watch()
   return (
     <section className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background">
       <TransformWrapper
@@ -19,7 +23,7 @@ export const ResumeContent = () => {
           <NavigationHeader title="Teste" />
           <TransformControls title="teste" />
           <TransformComponent>
-            <ResumeTemplate />
+            <ResumeTemplate data={data} />
           </TransformComponent>
         </>
       </TransformWrapper>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,23 @@
 import type { Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
-const config: Config = {
+const config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
@@ -17,16 +26,15 @@ const config: Config = {
         arial: ['Arial', ...fontFamily.sans],
       },
       colors: {
+        resume: {
+          primary: 'var(--resume-primary)',
+        },
+
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
@@ -34,6 +42,10 @@ const config: Config = {
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
         },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
@@ -43,19 +55,13 @@ const config: Config = {
           DEFAULT: 'hsl(var(--accent))',
           foreground: 'hsl(var(--accent-foreground))',
         },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
         },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
         },
       },
       borderRadius: {
@@ -63,8 +69,23 @@ const config: Config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
-}
+} satisfies Config
+
 export default config
